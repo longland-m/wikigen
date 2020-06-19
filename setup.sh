c0e7ddde3413f053608caec60cb3f9351db0836f
longland-m <54938016+longland-m@users.noreply.github.com>
2020-06-19 10:21:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-!wget http://ttic.uchicago.edu/~wieting/iclr-demo.zip
-!unzip iclr-demo.zip
-!rm iclr-demo.zip
-!mv iclr-demo/paragram_sl999_small.txt sif
-!rm iclr-demo/*
-!rm iclr-demo
+wget http://ttic.uchicago.edu/~wieting/iclr-demo.zip
+unzip iclr-demo.zip
+rm iclr-demo.zip
+mv iclr-demo/paragram_sl999_small.txt sif
+rm iclr-demo/*
+rm iclr-demo
